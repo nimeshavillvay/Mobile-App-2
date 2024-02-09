d2536277b7b212f8f6b0453ff133d67fab6e1a6e
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-02-09 19:46:41 +0530

    Added scroll buttons to carousels in home page

--- apps/lac-web/app/(home)/featured-products.tsx
+++ apps/lac-web/app/(home)/featured-products.tsx
@@ -6,12 +6,15 @@ import {
   ProductCardDetails,
 } from "@/_components/product-card";
 import Title from "@/_components/title";
+import VisuallyHidden from "@/_components/visually-hidden";
 import useAccountList from "@/_hooks/account/use-account-list.hook";
 import useLoginDialog from "@/_hooks/account/use-login-dialog.hook";
 import { cn, getMediaUrl } from "@/_utils/helpers";
 import * as Tabs from "@radix-ui/react-tabs";
+import { type EmblaCarouselType } from "embla-carousel";
 import useEmblaCarousel from "embla-carousel-react";
-import { useState } from "react";
+import { useCallback, useEffect, useState, type ComponentProps } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { IoPricetagsOutline } from "react-icons/io5";
 import type { FeaturedProduct } from "./types";
 
@@ -36,8 +39,40 @@ const FeaturedProducts = ({
   latest,
   special,
 }: FeaturedProductsProps) => {
-  const [emblaRef] = useEmblaCarousel({ loop: true });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const [selectedType, setSelectedType] = useState<ProductsType>("special");
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollPrev();
+    }
+  }, [emblaApi]);
+  const scrollNext = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollNext();
+    }
+  }, [emblaApi]);
+  const scrollTo = useCallback(
+    (index: number) => {
+      if (emblaApi) {
+        emblaApi.scrollTo(index);
+      }
+    },
+    [emblaApi],
+  );
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onSelect);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onSelect]);
 
   let productsList: FeaturedProduct[] = [];
   if (selectedType === "bestSellers") {
@@ -56,54 +91,86 @@ const FeaturedProducts = ({
   }
 
   return (
-    <section className="mb-[72px] mt-[55px]">
-      <Tabs.Root
-        value={selectedType}
-        onValueChange={(value) => setSelectedType(value as ProductsType)}
-      >
-        <div className="mb-7 flex flex-row justify-between">
-          <Title asChild>
-            <h2 className="border-brand-gray-400 flex-1 border-b pb-3.5">
-              Featured Products
-            </h2>
-          </Title>
+    <section className="full-bleed mb-[72px] mt-[55px]">
+      <div className="flex flex-row items-center justify-center gap-2">
+        <ScrollButton onClick={scrollPrev}>
+          <VisuallyHidden>Previous banner</VisuallyHidden>
+          <FaChevronLeft />
+        </ScrollButton>
 
-          <Tabs.List className="flex flex-row items-end">
-            {productTypes.map((productType) => (
-              <Tabs.Trigger
-                key={productType}
-                value={productType}
-                className="text-brand-gray-500 border-brand-gray-400 px-7 py-3 text-base font-bold capitalize leading-5 data-[state=active]:border-x data-[state=active]:border-t data-[state=inactive]:border-b data-[state=active]:text-black"
-              >
-                {productType.split(/(?=[A-Z])/).join(" ")}
-              </Tabs.Trigger>
-            ))}
-          </Tabs.List>
-        </div>
+        <Tabs.Root
+          value={selectedType}
+          onValueChange={(value) => setSelectedType(value as ProductsType)}
+          className="max-w-desktop"
+        >
+          <div className="mb-7 flex flex-row justify-between">
+            <Title asChild>
+              <h2 className="border-brand-gray-400 flex-1 border-b pb-3.5">
+                Featured Products
+              </h2>
+            </Title>
 
-        {productTypes.map((productType, rootIndex) => (
-          <Tabs.Content key={productType} value={productType} className="grid">
-            <div className="overflow-hidden" ref={emblaRef}>
-              <div className="flex">
-                {productsListPages.map((page, index) => (
-                  <div
-                    key={index}
-                    className="grid shrink-0 grow-0 basis-full grid-cols-4 gap-8"
-                  >
-                    {page.map((product) => (
-                      <FeaturedProduct
-                        key={product.sku}
-                        product={product}
-                        priority={rootIndex === 0 && index === 0}
-                      />
-                    ))}
-                  </div>
-                ))}
+            <Tabs.List className="flex flex-row items-end">
+              {productTypes.map((productType) => (
+                <Tabs.Trigger
+                  key={productType}
+                  value={productType}
+                  className="text-brand-gray-500 border-brand-gray-400 px-7 py-3 text-base font-bold capitalize leading-5 data-[state=active]:border-x data-[state=active]:border-t data-[state=inactive]:border-b data-[state=active]:text-black"
+                >
+                  {productType.split(/(?=[A-Z])/).join(" ")}
+                </Tabs.Trigger>
+              ))}
+            </Tabs.List>
+          </div>
+
+          {productTypes.map((productType, rootIndex) => (
+            <Tabs.Content
+              key={productType}
+              value={productType}
+              className="grid"
+            >
+              <div className="overflow-hidden" ref={emblaRef}>
+                <div className="flex">
+                  {productsListPages.map((page, index) => (
+                    <div
+                      key={index}
+                      className="grid shrink-0 grow-0 basis-full grid-cols-4 gap-8"
+                    >
+                      {page.map((product) => (
+                        <FeaturedProduct
+                          key={product.sku}
+                          product={product}
+                          priority={rootIndex === 0 && index === 0}
+                        />
+                      ))}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          </Tabs.Content>
+            </Tabs.Content>
+          ))}
+        </Tabs.Root>
+
+        <ScrollButton onClick={scrollNext}>
+          <VisuallyHidden>Next banner</VisuallyHidden>
+          <FaChevronRight />
+        </ScrollButton>
+      </div>
+
+      <div className="mt-[22px] flex items-center justify-center gap-1">
+        {productsListPages.map((page, index) => (
+          <button
+            key={index}
+            onClick={() => scrollTo(index)}
+            className={cn(
+              "h-1 w-9 shrink-0",
+              selectedIndex === index ? "bg-black/50" : " bg-black/15",
+            )}
+          >
+            <VisuallyHidden>Page {index}</VisuallyHidden>
+          </button>
         ))}
-      </Tabs.Root>
+      </div>
     </section>
   );
 };
@@ -178,5 +245,16 @@ const FeaturedProduct = ({
         </span>
       )}
     </ProductCardContainer>
+  );
+};
+
+const ScrollButton = (
+  props: Pick<ComponentProps<"button">, "children" | "onClick">,
+) => {
+  return (
+    <button
+      className="grid size-8 place-items-center bg-black/15 text-sm leading-none"
+      {...props}
+    />
   );
 };
