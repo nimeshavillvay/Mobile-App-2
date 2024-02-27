dbcf3223001baf04c423b34500262c968479bd1c
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-02-27 21:33:42 +0530

    Refactored streaming setup for TanStack Query

--- apps/lac-web/app/providers.tsx
+++ apps/lac-web/app/providers.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import useCookies from "@/old/_hooks/storage/use-cookies.hook";
-import {
-  ACCOUNT_NO_COOKIE,
-  ADDRESS_ID_COOKIE,
-  TOKEN_COOKIE,
-} from "@/old/_lib/constants";
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-import { useState, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { CookiesProvider } from "react-cookie";
-import useLogout from "./(old-design)/_hooks/account/use-logout.hook";
-import { selectAccount } from "./(old-design)/_lib/shared-apis";
 
 type ProvidersProps = {
   children: ReactNode;
@@ -32,65 +20,36 @@ const Providers = ({ children }: ProvidersProps) => {
 
 export default Providers;
 
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 600000, // 10 mins
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
+
 const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
-  const [isRefreshing, setIsRefreshing] = useState(false);
-  const [cookies, setCookies] = useCookies();
-  const logout = useLogout();
-
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 600000, // 10 mins
-            retry: (failureCount) => {
-              // Don't retry for certain error responses
-              // Temporarily disable check due to this error
-              // https://github.com/sindresorhus/ky/issues/513
-              // if (error?.response?.status === 401) {
-              //   return false;
-              // }
-
-              // Retry others just once
-              return failureCount <= 1;
-            },
-          },
-        },
-        queryCache: new QueryCache({
-          onError: async () => {
-            // Refresh the account token
-            if (!isRefreshing) {
-              setIsRefreshing(true);
-
-              if (
-                // Temporarily disable check due to this error
-                // https://github.com/sindresorhus/ky/issues/513
-                // error?.response?.status === 401 &&
-                cookies[TOKEN_COOKIE] &&
-                cookies[ACCOUNT_NO_COOKIE] &&
-                cookies[ADDRESS_ID_COOKIE]
-              ) {
-                try {
-                  const { token } = await selectAccount(
-                    cookies[TOKEN_COOKIE],
-                    cookies[ACCOUNT_NO_COOKIE],
-                    cookies[ADDRESS_ID_COOKIE],
-                  );
-                  setCookies("account-token", token, { path: "/" });
-                } catch {
-                  // Completely logout if refresh account token fails
-                  logout();
-                }
-              } else {
-                logout();
-              }
-
-              setIsRefreshing(false);
-            }
-          },
-        }),
-      }),
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
