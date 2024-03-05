a18e295c2f6e85aa740b77211c17c0a3fe865d03
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-03-05 09:25:22 +0530

    Create base button component

--- packages/web-ui/src/components/ui/button/button.tsx
+++ packages/web-ui/src/components/ui/button/button.tsx
@@ -1,44 +1,42 @@
 import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react";
+import { type VariantProps } from "cva";
+import { forwardRef, type ButtonHTMLAttributes } from "react";
+import { cva } from "../../../lib/cva.config";
 import { cn } from "../../../lib/utils";
 
-const buttonVariants = cva(
-  "ui-inline-flex ui-items-center ui-justify-center ui-whitespace-nowrap ui-rounded-md ui-text-sm ui-font-medium ui-ring-offset-white ui-transition-colors focus-visible:ui-outline-none focus-visible:ui-ring-2 focus-visible:ui-ring-gray-950 focus-visible:ui-ring-offset-2 disabled:ui-pointer-events-none disabled:ui-opacity-50",
-  {
-    variants: {
-      variant: {
-        default: "ui-bg-gray-900 ui-text-gray-50 hover:ui-bg-gray-900/90",
-        destructive: "ui-bg-red-500 ui-text-gray-50 hover:ui-bg-red-500/90",
-        outline:
-          "ui-border ui-border-gray-200 ui-bg-white hover:ui-bg-gray-100 hover:ui-text-gray-900",
-        secondary: "ui-bg-gray-100 ui-text-gray-900 hover:ui-bg-gray-100/80",
-        ghost: "hover:ui-bg-gray-100 hover:ui-text-gray-900",
-        link: "ui-text-gray-900 ui-underline-offset-4 hover:ui-underline",
-      },
-      size: {
-        default: "ui-h-10 ui-px-4 ui-py-2",
-        sm: "ui-h-9 ui-rounded-md ui-px-3",
-        lg: "ui-h-11 ui-rounded-md ui-px-8",
-        icon: "ui-h-10 ui-w-10",
-      },
+const buttonVariants = cva({
+  base: "ui-inline-flex ui-items-center ui-justify-center ui-whitespace-nowrap ui-text-sm ui-font-semibold ui-border-2 disabled:ui-pointer-events-none disabled:ui-opacity-50",
+  variants: {
+    variant: {
+      default:
+        "ui-bg-black ui-border-black hover:ui-bg-zinc-800 hover:ui-border-zinc-800 focus:ui-bg-zinc-800 focus:ui-border-black ui-text-white",
+      destructive:
+        "ui-bg-red-650 ui-border-red-650 hover:ui-bg-red-800 hover:ui-border-red-800 focus:ui-bg-red-800 focus:ui-border-[#0E0E13]/25 ui-text-white",
+      outline:
+        "ui-bg-white ui-border-[#0E0E13]/[8%] hover:ui-bg-zinc-100 hover:ui-border-[#0E0E13]/15 focus:ui-border-[#0E0E13]/50 focus:ui-bg-zinc-100 ui-text-zinc-950",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      small: "ui-rounded-lg ui-py-3 ui-px-4 ui-gap-2",
+      medium: "ui-rounded-lg ui-py-3 ui-px-6 ui-gap-3",
+      large: "ui-rounded-xl ui-py-4 ui-px-8 ui-gap-4",
     },
   },
-);
+  defaultVariants: {
+    variant: "default",
+    size: "medium",
+  },
+});
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
--- packages/web-ui/tailwind.config.ts
+++ packages/web-ui/tailwind.config.ts
@@ -26,6 +26,11 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        red: {
+          650: "#BB271A",
+        },
+      },
     },
   },
   plugins: [],
