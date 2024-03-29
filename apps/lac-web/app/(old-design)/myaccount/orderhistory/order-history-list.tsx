e33570d78d236309954ba6647f2ef92f0f2f410c
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-03-29 09:48:20 +0530

    WXM-14: Initialize my orders section

---
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/order-history-list.tsx
@@ -0,0 +1,10 @@
+const OrderHistoryList = ({ token }: { token: string }) => {
+  console.log("token >", token);
+  return (
+    <div className="max-w-desktop text-wrap text-brand-success">
+      Order History List Here
+    </div>
+  );
+};
+
+export default OrderHistoryList;
--- apps/lac-web/app/(old-design)/myaccount/orderhistory/page.tsx
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/page.tsx
@@ -1,13 +1,32 @@
+import Separator from "@/old/_components/separator";
+import { ACCOUNT_TOKEN_COOKIE } from "@/old/_lib/constants";
 import type { Metadata } from "next";
+import { cookies } from "next/headers";
+import OrderHistoryList from "./order-history-list";
 
 export const metadata: Metadata = {
-  title: "Order History",
+  title: "My Orders",
 };
 
 const OrderHistoryPage = () => {
+  const accountTokenCookie = cookies().get(ACCOUNT_TOKEN_COOKIE);
+
+  if (!accountTokenCookie?.value) {
+    return null;
+  }
+
   return (
     <>
-      <h2>Order History</h2>
+      <h2 className="relative font-wurth text-xl font-medium text-brand-primary">
+        My Orders
+      </h2>
+
+      <Separator
+        orientation="horizontal"
+        className="mb-4 h-px flex-1 bg-brand-primary"
+      />
+
+      <OrderHistoryList token={accountTokenCookie?.value} />
     </>
   );
 };
