cecdb21188be1efca19227fa2b0abeaedc219c03
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-03-29 12:26:06 +0530

    WXM-14: Add inital use suspense quary

--- apps/lac-web/app/(old-design)/myaccount/orderhistory/order-history-list.tsx
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/order-history-list.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import dayjs from "dayjs";
+import { DATE_FORMAT } from "./constants";
+import useSuspenseOrderHistoryList from "./use-suspense-order-history-list.hook";
+
 const OrderHistoryList = ({ token }: { token: string }) => {
-  console.log("token >", token);
+  const initialFromDate = dayjs().subtract(1, "year").format(DATE_FORMAT);
+  const initialToDate = dayjs().format(DATE_FORMAT);
+
+  const orderHistoryListQuery = useSuspenseOrderHistoryList(
+    token,
+    initialFromDate,
+    initialToDate,
+    "",
+    "desc",
+    "orderDate",
+    0,
+    10,
+  );
+
+  console.log("oh items >", orderHistoryListQuery.data);
+
   return (
     <div className="max-w-desktop text-wrap text-brand-success">
       Order History List Here
---
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/use-suspense-order-history-list.hook.ts
@@ -0,0 +1,44 @@
+import { api } from "@/old/_lib/api";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+const useSuspenseOrderHistoryList = (
+  token: string,
+  fromDate: string,
+  toDate: string,
+  orderNumber: string,
+  orderType: string,
+  sortBy: string,
+  currentPage: number,
+  pageSize: number,
+) => {
+  return useSuspenseQuery({
+    queryKey: [
+      "user",
+      "my-orders",
+      token,
+      orderNumber,
+      orderType,
+      sortBy,
+      currentPage,
+      pageSize,
+    ],
+    queryFn: () =>
+      api
+        .get("am/order-history/search", {
+          headers: {
+            authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          searchParams: {
+            orderNo: orderNumber,
+            page: currentPage,
+            size: pageSize,
+            sort: sortBy,
+            order: orderType,
+          },
+        })
+        .json<unknown>(),
+  });
+};
+
+export default useSuspenseOrderHistoryList;
