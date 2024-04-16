c6c77ea7b1b519304f2fd341980c68bcf944f1eb
lahiru ariyarathna <lahiru.ariyarathna@villvay.com>
2024-04-16 18:19:58 +0530

    get user mapper done

--- apps/lac-web/app/(old-design)/myaccount/manage-users/use-suspense-users-list.hook.ts
+++ apps/lac-web/app/(old-design)/myaccount/manage-users/use-suspense-users-list.hook.ts
@@ -1,19 +1,58 @@
-import { api } from "@/old/_lib/api";
+import { api } from "@/_lib/api";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import type { ManageUsers } from "./types";
+
+type Contact = {
+  id: string;
+  status: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  role: string;
+  role_description: string;
+  permission: string;
+  sold_to_status: string;
+};
+
+type ManageContact = {
+  manage_contact: {
+    your_profile: Contact;
+    contact_list: [Contact];
+  };
+};
+
+const mapContact = (contact: Contact) => ({
+  id: Number(contact.id),
+  status: contact.status,
+  firstName: contact.first_name,
+  lastName: contact.last_name,
+  email: contact.email,
+  role: contact.role,
+  roleDescription: contact.role_description,
+  permission: contact.permission,
+  soldToAccountStatus: contact.sold_to_status,
+});
 
 const useSuspenseUsersList = (token: string) => {
   return useSuspenseQuery({
     queryKey: ["user", "manage-users", token],
     queryFn: () =>
       api
-        .get("am/manage_users", {
+        .get("rest/my-account/users", {
           headers: {
             authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
         })
-        .json<ManageUsers>(),
+        .json<ManageContact>(),
+    select: (data) => {
+      const { your_profile, contact_list } = data.manage_contact;
+      return {
+        manageContact: {
+          yourProfile: mapContact(your_profile),
+          contactList: contact_list.map(mapContact),
+        },
+      };
+    },
   });
 };
 
