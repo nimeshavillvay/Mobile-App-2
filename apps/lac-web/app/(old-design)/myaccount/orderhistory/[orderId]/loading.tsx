fa2558360a66372cf32268cf1f97d6cda31407c2
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-04-25 15:57:42 +0530

    WXM-14: Data binding

---
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/[orderId]/loading.tsx
@@ -0,0 +1,9 @@
+const DetailedOrderLoading = () => {
+  return (
+    <div>
+      <h1>Order Details Loading...</h1>
+    </div>
+  );
+};
+
+export default DetailedOrderLoading;
--- apps/lac-web/app/(old-design)/myaccount/orderhistory/[orderId]/page.tsx
+++ apps/lac-web/app/(old-design)/myaccount/orderhistory/[orderId]/page.tsx
@@ -1,6 +1,13 @@
+import AlertInline from "@/old/_components/alert-inline";
+import Separator from "@/old/_components/separator";
+import { Badge } from "@/old/_components/ui/badge";
+import { formatNumberToPrice } from "@/old/_utils/helpers";
+import dayjs from "dayjs";
 import type { Metadata } from "next";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import BackButton from "../back-button";
+import { getOrderDetail } from "./api";
 
 type DetailedOrderPageProps = {
   params: {
@@ -22,12 +29,183 @@ export const generateMetadata = async ({
   };
 };
 
-const DetailedOrderPage = ({ params: { orderId } }: DetailedOrderPageProps) => {
+const DetailedOrderPage = async ({
+  params: { orderId },
+}: DetailedOrderPageProps) => {
+  const orderDetail = await getOrderDetail(orderId);
+
+  const orderTrackingHref = orderDetail.orderNo
+    ? `/myaccount/orderhistory/ordertrackinglog/${orderDetail.orderNo}`
+    : "#";
+
+  console.log(orderDetail);
+
   return (
     <>
-      <BackButton title="Back to My Orders" />
-      <h1>Order Details {orderId}</h1>
-      <p>This is the detailed order page</p>
+      <BackButton title="Back to My Orders" className="mb-2" />
+
+      <h2 className="relative font-wurth text-xl font-medium text-brand-primary">
+        Order #{orderId}
+      </h2>
+
+      <Separator
+        orientation="horizontal"
+        className="mb-2.5 h-px flex-1 bg-brand-primary"
+      />
+
+      <div className="mt-2 space-y-4">
+        {/* Order Details Section */}
+        <div className="grid w-full grid-cols-1 text-brand-gray-500 md:grid-cols-4">
+          <div className="col-span-1 md:col-span-2">
+            <div className="mb-1 font-bold">Document Status</div>
+
+            <Badge className="mb-4 border-brand-secondary px-2 py-0.5 capitalize text-brand-secondary">
+              {orderDetail?.orderStatus ?? "N/A"}
+            </Badge>
+
+            <div>
+              <div className="flex flex-row">
+                <div className="flex-1 font-bold">Account No:</div>
+                <div className="flex-1">{orderDetail?.soldTo ?? "N/A"}</div>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex-1 font-bold">Order By:</div>
+                <div className="flex-1 truncate whitespace-nowrap">
+                  {orderDetail?.orderBy ?? "N/A"}
+                </div>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex-1 font-bold">Email:</div>
+                <div className="flex-1 truncate whitespace-nowrap">
+                  {orderDetail?.email ?? "N/A"}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="font-bold">Billing Address</div>
+            <div>{orderDetail?.billToAddress?.attention ?? ""}</div>
+            <div>{orderDetail?.billToAddress?.street ?? ""}</div>
+            <div>{orderDetail?.billToAddress?.city ?? ""}</div>
+            <div>
+              {orderDetail?.billToAddress?.region ?? ""}&nbsp;
+              {orderDetail?.billToAddress?.zipCode ?? ""}
+            </div>
+            <div>Phone : {orderDetail?.billToAddress?.phoneNumber ?? ""}</div>
+          </div>
+
+          <div>
+            <div className="font-bold">Shipping Address</div>
+            <div>{orderDetail?.shipToAddress?.attention ?? ""}</div>
+            <div>{orderDetail?.shipToAddress?.street ?? ""}</div>
+            <div>{orderDetail?.shipToAddress?.city ?? ""}</div>
+            <div>
+              {orderDetail?.shipToAddress?.region ?? ""}&nbsp;
+              {orderDetail?.shipToAddress?.zipCode ?? ""}
+            </div>
+            <div>Phone : {orderDetail?.shipToAddress?.phoneNumber ?? ""}</div>
+          </div>
+        </div>
+
+        <Separator
+          orientation="horizontal"
+          className="h-px flex-1 bg-brand-gray-200"
+        />
+
+        <div className="grid grid-cols-2 text-brand-gray-500">
+          <div>
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">Order Date:</div>
+              <div className="flex-1">
+                {orderDetail?.orderDate !== ""
+                  ? dayjs(orderDetail.orderDate).format("MM/DD/YYYY")
+                  : "N/A"}
+              </div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">Job Name:</div>
+              <div className="flex-1">{orderDetail?.jobName ?? "N/A"}</div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">PO#:</div>
+              <div className="flex-1">{orderDetail?.po ?? "N/A"}</div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">Payment Method:</div>
+              <div className="flex-1">
+                {orderDetail?.paymentMethod ?? "N/A"}
+              </div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">Promo Code:</div>
+              <div className="flex-1">
+                {orderDetail?.promoCode !== "" ? orderDetail.promoCode : "N/A"}
+              </div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="flex-1 font-bold">Driver Note:</div>
+              <div className="flex-1">
+                {orderDetail?.driverNotes !== ""
+                  ? orderDetail.driverNotes
+                  : "N/A"}
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-1.5">
+            <div className="flex flex-row items-center justify-between">
+              <div>Shipping & Handing</div>
+              <div>${formatNumberToPrice(orderDetail.handlingFee)}</div>
+            </div>
+
+            <Separator
+              orientation="horizontal"
+              className="h-px flex-1 bg-brand-gray-200"
+            />
+
+            <div className="flex flex-row items-center justify-between">
+              <div>Sub Total</div>
+              <div>${formatNumberToPrice(orderDetail.subTotal)}</div>
+            </div>
+
+            <div className="flex flex-row items-center justify-between">
+              <div>Sales Tax</div>
+              <div>${formatNumberToPrice(orderDetail.taxAmount)}</div>
+            </div>
+
+            <Separator
+              orientation="horizontal"
+              className="h-0.5 flex-1 bg-brand-gray-200"
+            />
+
+            <div className="flex flex-row items-center justify-between font-bold">
+              <div>Order Total</div>
+              <div>${formatNumberToPrice(orderDetail.orderTotal)}</div>
+            </div>
+          </div>
+        </div>
+
+        <Link
+          href={orderTrackingHref}
+          className="block w-fit rounded-sm bg-brand-secondary px-4 py-2 text-center font-wurth text-base font-extrabold uppercase text-white"
+        >
+          Order Tracking Log
+        </Link>
+
+        {!orderDetail.completeDelivery && (
+          <AlertInline
+            variant="destructive"
+            title="Important!"
+            description="This order will not ship until back order item(s) are in stock."
+          />
+        )}
+      </div>
     </>
   );
 };
