eac78cd686b5b4b4be7ac0169c269791c83e7a0d
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-02 00:30:45 +0530

    Refactor build config

--- packages/web-ui/tsup.config.ts
+++ packages/web-ui/tsup.config.ts
@@ -1,4 +1,5 @@
 import react18Plugin from "esbuild-plugin-react18";
+import { globSync } from "glob";
 import { defineConfig, Options } from "tsup";
 
 // To get rid of the `Error [ERR_WORKER_OUT_OF_MEMORY]: Worker terminated due to reaching memory limit: JS heap out of memory`
@@ -6,7 +7,6 @@ import { defineConfig, Options } from "tsup";
 // https://github.com/egoist/tsup/issues/920#issuecomment-1791496481
 
 const baseConfig: Options = {
-  clean: true,
   format: ["esm"],
   external: [
     "react",
@@ -21,10 +21,14 @@ const baseConfig: Options = {
   esbuildPlugins: [react18Plugin()],
 };
 
+// Get paths of all icon components
+const iconComponents = globSync("src/components/icons/**/*/index.ts");
+
 export default defineConfig([
   // Bundle JS together, to make sure code is shared as much as possible
   {
     ...baseConfig,
+    clean: true,
     entry: [
       "src/components/icons/**/*/index.ts",
       "src/components/logos/**/*/index.ts",
@@ -35,14 +39,6 @@ export default defineConfig([
   // Generate types separately as generating for everything requires too much
   // compute and memory resulting in our developer machines freezing and failing
   // on CI/CD.
-  {
-    ...baseConfig,
-    entry: ["src/components/icons/**/*/index.ts"],
-    outDir: "dist/icons",
-    dts: {
-      only: true,
-    },
-  },
   {
     ...baseConfig,
     entry: ["src/components/logos/**/*/index.ts"],
@@ -67,4 +63,12 @@ export default defineConfig([
       only: true,
     },
   },
+  ...iconComponents.map((iconPath) => ({
+    ...baseConfig,
+    entry: [iconPath],
+    outDir: iconPath.replace("src/components", "dist").replace("/index.ts", ""),
+    dts: {
+      only: true,
+    },
+  })),
 ]);
