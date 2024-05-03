1ec0c620e51c58ea71e70b167e22008a624a8a35
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-03 15:19:40 +0530

    Added icon to error page

--- apps/lac-web/app/error.tsx
+++ apps/lac-web/app/error.tsx
@@ -19,6 +19,30 @@ const ErrorPage = ({
   return (
     <div className="grid size-full place-items-center">
       <div className="mb-32 mt-28 text-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="96"
+          height="88"
+          fill="none"
+          viewBox="0 0 96 88"
+          className="mx-auto mb-[42px]"
+        >
+          <path
+            stroke="#BDBDBD"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="3"
+            d="M92.062 71.716L55.835 8.17a9.067 9.067 0 00-3.32-3.368A9.04 9.04 0 0040.078 8.17L3.85 71.716a9.094 9.094 0 003.368 12.432 9.04 9.04 0 004.557 1.185h72.453a9.04 9.04 0 004.525-1.22 9.092 9.092 0 003.309-12.397zM48 18.871V62.88"
+          />
+          <path
+            stroke="#BDBDBD"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="4"
+            d="M48 74.392h.045"
+          />
+        </svg>
+
         <h1 className="font-title text-2xl font-bold capitalize leading-7 text-wurth-gray-800">
           Unexpected Error
         </h1>
