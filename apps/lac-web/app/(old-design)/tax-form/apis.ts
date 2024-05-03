d00e475cfec9c1c899ae1047e3307d6a4c74f8a1
Ishan Gunawardhana <ishan@villvay.com>
2024-05-03 13:34:22 +0530

    Update tax-form page for real apis.

--- apps/lac-web/app/(old-design)/tax-form/apis.ts
+++ apps/lac-web/app/(old-design)/tax-form/apis.ts
@@ -1,10 +1,17 @@
 import { api } from "@/_lib/api";
-import { DEFAULT_REVALIDATE } from "@/_lib/constants";
+import { DEFAULT_REVALIDATE, SESSION_TOKEN_COOKIE } from "@/_lib/constants";
+import { cookies } from "next/headers";
 import type { TaxFormItems } from "./types";
 
 export const getTaxFormDetails = async () => {
+  const cookieStore = cookies();
+  const tokenCookie = cookieStore.get(SESSION_TOKEN_COOKIE);
+
   return api
     .get("rest/docs/tax-forms", {
+      headers: {
+        Authorization: `Bearer ${tokenCookie?.value}`,
+      },
       next: {
         revalidate: DEFAULT_REVALIDATE,
       },
