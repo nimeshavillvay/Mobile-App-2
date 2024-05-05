b7ae1a41070d1dc0871f8bf4cb82989421ac3bd4
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-05 16:44:10 +0530

    Don't cache OG image

--- apps/lac-web/app/product/[id]/[slug]/opengraph-image.tsx
+++ apps/lac-web/app/product/[id]/[slug]/opengraph-image.tsx
@@ -1,4 +1,5 @@
 import OGImage from "@/_lib/og-image";
+import { unstable_noStore as noStore } from "next/cache";
 import { getProduct } from "./apis";
 import type { ProductPageProps } from "./types";
 
@@ -11,6 +12,7 @@ export const runtime = "edge";
 export const contentType = "image/png";
 
 const Image = async ({ params: { id, slug } }: ProductPageProps) => {
+  noStore(); // Added this because the cache gets confused between the product and category pages
   const product = await getProduct(id, slug);
 
   return OGImage({
