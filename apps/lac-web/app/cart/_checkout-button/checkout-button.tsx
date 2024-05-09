1c0ed7b73eb06c61373829089ec99e9994ec7ccd
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-09 16:06:59 +0530

    Refactored checkout button

--- apps/lac-web/app/cart/_checkout-button/checkout-button.tsx
+++ apps/lac-web/app/cart/_checkout-button/checkout-button.tsx
@@ -1,33 +1,41 @@
 "use client";
 
+import useSuspenseSimulationCheckout from "@/_hooks/cart/use-suspense-simulation-checkout.hook";
 import useSuspenseCheckLogin from "@/_hooks/user/use-suspense-check-login.hook";
 import { Shield } from "@repo/web-ui/components/icons/shield";
-import { buttonVariants } from "@repo/web-ui/components/ui/button";
-import Link from "next/link";
+import { Button } from "@repo/web-ui/components/ui/button";
+import { useRouter } from "next/navigation";
 
 type CheckoutButtonProps = {
   token: string;
 };
 
 const CheckoutButton = ({ token }: CheckoutButtonProps) => {
+  const router = useRouter();
+
   const checkLoginQuery = useSuspenseCheckLogin(token);
+  const simulationCheckoutQuery = useSuspenseSimulationCheckout(token);
+
+  const handleClick = () => {
+    if (checkLoginQuery.data.status_code === "OK") {
+      router.push("/checkout");
+    } else {
+      router.push("/sign-in");
+    }
+  };
 
   return (
-    <Link
-      href={
-        checkLoginQuery.data.status_code === "OK" ? "/checkout" : "/sign-in"
-      }
-      className={buttonVariants({
-        variant: "secondary",
-        size: "lg",
-        className:
-          "h-fit w-full gap-2 rounded-lg px-5 py-4 text-lg font-normal shadow-md",
-      })}
+    <Button
+      variant="secondary"
+      size="lg"
+      className="h-fit w-full gap-2 rounded-lg px-5 py-4 text-lg font-normal shadow-md"
+      onClick={handleClick}
+      disabled={simulationCheckoutQuery.data.totalQuantity === 0}
     >
       <Shield className="size-5 stroke-white" />
 
       <span>Secure Checkout</span>
-    </Link>
+    </Button>
   );
 };
 
