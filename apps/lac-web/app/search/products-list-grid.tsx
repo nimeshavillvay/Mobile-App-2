f2473f9f8a9ce64b7aa9003311fbc03e028fad4e
Frank Alwis <frank.alwis@villvay.com>
2024-05-10 17:13:11 +0530

    added popup

--- apps/lac-web/app/search/products-list-grid.tsx
+++ apps/lac-web/app/search/products-list-grid.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { ProductsGridList } from "@/_components/products-grid";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+} from "@repo/web-ui/components/ui/dialog";
 import { useRouter } from "next/navigation";
 import { type ComponentProps } from "react";
 import useSuspenseSearchProductList from "./use-suspense-search-product-list.hook";
@@ -15,6 +21,34 @@ type ProductListGridProps = {
 const ProductListGrid = ({ type, term, pageNo }: ProductListGridProps) => {
   const router = useRouter();
   const searchQuery = useSuspenseSearchProductList(term, pageNo);
+
+  const handleCloseDialog = () => {
+    router.push("/");
+  };
+
+  const handleOpenChange = (newOpen: boolean) => {
+    if (!newOpen) {
+      handleCloseDialog(); // Redirect when dialog is closed
+    }
+  };
+
+  if (
+    searchQuery.data.results &&
+    Array.isArray(searchQuery.data.results) &&
+    searchQuery.data.results.length === 0
+  ) {
+    return (
+      <Dialog open={true} onOpenChange={handleOpenChange}>
+        <DialogOverlay />
+        <DialogContent>
+          <h2>No Results Found</h2>
+          <p>Sorry, no results were found for your search term.</p>
+          <DialogClose />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   const handleRedirect = () => {
     if (searchQuery.data.summary.plp == true) {
       const productPath = `/product/${searchQuery.data.results.id}/${searchQuery.data.results.slug}`;
