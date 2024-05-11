f77d76f98db45678058b471aaa8f15f004561784
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-11 21:24:36 +0530

    Remove More to consider

--- apps/lac-web/app/product/[id]/[slug]/page.tsx
+++ apps/lac-web/app/product/[id]/[slug]/page.tsx
@@ -10,7 +10,6 @@ import {
   ProductCardDiscount,
   ProductCardHero,
   ProductCardImage,
-  ProductCardLabel,
   ProductCardPrice,
 } from "@repo/web-ui/components/product-card";
 import {
@@ -205,41 +204,6 @@ const ProductPage = async ({ params: { id, slug } }: ProductPageProps) => {
           ))}
         </section>
       )}
-
-      <section className="my-10 space-y-6 bg-wurth-gray-50 py-10 md:space-y-5">
-        <h2 className="container text-center font-title text-2xl font-medium tracking-[-0.144px] text-wurth-gray-800">
-          More to consider
-        </h2>
-
-        <div className="container flex w-full snap-x scroll-pl-4 flex-row gap-5 overflow-x-auto md:scroll-pl-8">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <ProductCard key={index} className="shrink-0 snap-start">
-              <ProductCardHero>
-                <ProductCardDiscount>30</ProductCardDiscount>
-
-                <ProductCardImage
-                  src={productItemImage}
-                  alt="A placeholder product"
-                  href={"/product/12345"}
-                  title='Pro 128mm Mod Bar Pull, Satin Champagne, 5-11/16" Length'
-                />
-
-                <ProductCardLabel>Label</ProductCardLabel>
-              </ProductCardHero>
-
-              <ProductCardContent>
-                <ProductCardDetails
-                  title='Pro 128mm Mod Bar Pull, Satin Champagne, 5-11/16" Length'
-                  sku="PROMD8-SCP"
-                  href="/product/771770/PROMD3-MB"
-                />
-
-                <ProductCardPrice price={2.05} uom="pair" actualPrice={4.11} />
-              </ProductCardContent>
-            </ProductCard>
-          ))}
-        </div>
-      </section>
     </>
   );
 };
