4edde2f5fa3e99b2ead6906de4b29a7d42de6d3a
Frank Alwis <frank.alwis@villvay.com>
2024-05-14 19:08:22 +0530

    reverted to normal size

--- apps/lac-web/app/_header/header.tsx
+++ apps/lac-web/app/_header/header.tsx
@@ -116,7 +116,7 @@ const Header = async () => {
           <span className="sr-only">Home</span>
         </Link>
 
-        <div className="container relative w-[850px]">
+        <div className="container relative w-[800px]">
           <SearchBar />
         </div>
 
