4dba49d452720748bc59c56a570bf25493b34f9f
ilmanvillvay <ilman@villvay.com>
2024-05-16 16:56:18 +0530

    [WXM-327] comment out favorites filters

--- apps/lac-web/app/(old-design)/myaccount/_side-menu/favorites-filters.tsx
+++ apps/lac-web/app/(old-design)/myaccount/_side-menu/favorites-filters.tsx
@@ -2,12 +2,12 @@ import useSuspenseFilters from "@/_hooks/search/use-suspense-filters.hook";
 import FiltersDropdown from "./filters-dropdown";
 
 const FavoritesFilters = ({ token }: { token: string }) => {
-  const filtersQuery = useSuspenseFilters(token, {
-    type: "Favorites",
-    id: "0", // TODO: Replace this with favorites list id
-  });
-
-  return <FiltersDropdown filters={filtersQuery.data} />;
+  // TODO: This feature is not supported
+  // const filtersQuery = useSuspenseFilters(token, {
+  //   type: "Favorites",
+  //   id: "0", // TODO: Replace this with favorites list id
+  // });
+  // return <FiltersDropdown filters={filtersQuery.data} />;
 };
 
 export default FavoritesFilters;
