c615b3cf086285c658abf5e798c2c3528015389e
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-21 14:44:17 +0530

    WXM-369: Hide "Check Other Stores" when logged out

--- apps/lac-web/app/product/[id]/[slug]/_product-hero/_location-stocks/location-stocks.tsx
+++ apps/lac-web/app/product/[id]/[slug]/_product-hero/_location-stocks/location-stocks.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSuspenseCheckAvailability from "@/_hooks/product/use-suspense-check-availability.hook";
+import useSuspenseCheckLogin from "@/_hooks/user/use-suspense-check-login.hook";
 import { ChevronRight } from "@repo/web-ui/components/icons/chevron-right";
 import { Button } from "@repo/web-ui/components/ui/button";
 import {
@@ -23,6 +24,8 @@ const LocationStocks = ({ token, productId }: LocationStocksProps) => {
   const otherLocations =
     checkAvailabilityQuery.data.availableLocations.slice(1);
 
+  const checkLoginQuery = useSuspenseCheckLogin(token);
+
   return (
     <Collapsible className="flex flex-col gap-1">
       <div className="space-y-2 py-1 md:flex md:flex-row md:items-center md:justify-between md:space-y-0">
@@ -36,49 +39,53 @@ const LocationStocks = ({ token, productId }: LocationStocksProps) => {
           </div>
         </div>
 
-        <CollapsibleTrigger
-          className="group flex h-fit w-full flex-row items-center justify-between font-bold md:w-fit md:px-2 md:py-0.5"
-          asChild
-        >
-          <Button type="button" variant="subtle">
-            <span>Check Other Stores</span>
+        {checkLoginQuery.data.status_code === "OK" && (
+          <CollapsibleTrigger
+            className="group flex h-fit w-full flex-row items-center justify-between font-bold md:w-fit md:px-2 md:py-0.5"
+            asChild
+          >
+            <Button type="button" variant="subtle">
+              <span>Check Other Stores</span>
 
-            <ChevronRight
-              width={16}
-              height={16}
-              className="transition duration-150 ease-out group-data-[state=open]:rotate-90"
-            />
-          </Button>
-        </CollapsibleTrigger>
+              <ChevronRight
+                width={16}
+                height={16}
+                className="transition duration-150 ease-out group-data-[state=open]:rotate-90"
+              />
+            </Button>
+          </CollapsibleTrigger>
+        )}
       </div>
 
-      <CollapsibleContent>
-        <table className="w-full border-separate rounded-lg border border-wurth-gray-150 [&_td]:p-3 [&_th]:p-3">
-          <thead className="">
-            <tr className="text-sm text-wurth-gray-500">
-              <th className="border-b border-b-wurth-gray-150 text-left font-normal">
-                Location
-              </th>
-              <th className="border-b border-b-wurth-gray-150 text-right font-normal">
-                Stock
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {otherLocations.map((location) => (
-              <tr
-                key={location.location}
-                className="text-sm text-wurth-gray-800 [&:not(:last-child)]:border-b [&:not(:last-child)]:border-b-wurth-gray-150"
-              >
-                <td className="text-left font-normal">{location.name}</td>
-
-                <td className="text-right font-normal">{location.amount}</td>
+      {checkLoginQuery.data.status_code === "OK" && (
+        <CollapsibleContent>
+          <table className="w-full border-separate rounded-lg border border-wurth-gray-150 [&_td]:p-3 [&_th]:p-3">
+            <thead>
+              <tr className="text-sm text-wurth-gray-500">
+                <th className="border-b border-b-wurth-gray-150 text-left font-normal">
+                  Location
+                </th>
+                <th className="border-b border-b-wurth-gray-150 text-right font-normal">
+                  Stock
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </CollapsibleContent>
+            </thead>
+
+            <tbody>
+              {otherLocations.map((location) => (
+                <tr
+                  key={location.location}
+                  className="text-sm text-wurth-gray-800 [&:not(:last-child)]:border-b [&:not(:last-child)]:border-b-wurth-gray-150"
+                >
+                  <td className="text-left font-normal">{location.name}</td>
+
+                  <td className="text-right font-normal">{location.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </CollapsibleContent>
+      )}
     </Collapsible>
   );
 };
