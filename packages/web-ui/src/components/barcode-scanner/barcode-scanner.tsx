8e5a2a015ec48b1e0fd7081ef11f5d08a2aaf4df
Shyamali <shyamali@villvay.com>
2024-05-21 09:59:30 +0530

    fix error message displayed in slow network

--- packages/web-ui/src/components/barcode-scanner/barcode-scanner.tsx
+++ packages/web-ui/src/components/barcode-scanner/barcode-scanner.tsx
@@ -1,5 +1,5 @@
 import { BrowserMultiFormatReader } from "@zxing/library";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const BarcodeScanner = ({
   onScanSuccess,
@@ -9,6 +9,7 @@ export const BarcodeScanner = ({
   const videoRef = useRef<HTMLVideoElement>(null);
   const reader = useRef(new BrowserMultiFormatReader());
   const onScanSuccessRef = useRef(onScanSuccess);
+  const [oldQuery, setOldQuery] = useState("");
 
   useEffect(() => {
     onScanSuccessRef.current = onScanSuccess;
@@ -26,15 +27,16 @@ export const BarcodeScanner = ({
       },
       videoRef.current,
       (result) => {
-        if (result) {
+        if (result && result.getText() && result.getText() !== oldQuery) {
           onScanSuccessRef.current(result.getText());
+          setOldQuery(result.getText());
         }
       },
     );
     return () => {
       currentReader.reset();
     };
-  }, [videoRef]);
+  }, [videoRef, oldQuery]);
 
   return (
     <video width="100%" height="100%" ref={videoRef}>
