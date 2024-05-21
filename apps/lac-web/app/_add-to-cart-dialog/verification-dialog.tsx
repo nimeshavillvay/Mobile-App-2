fb91251a1c330f97999b4a729b9d0b521e418feb
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-05-21 19:56:48 +0530

    WXM-381: Show label when backordered

--- apps/lac-web/app/_add-to-cart-dialog/verification-dialog.tsx
+++ apps/lac-web/app/_add-to-cart-dialog/verification-dialog.tsx
@@ -11,7 +11,6 @@ import { AddToCart as AddToCartIcon } from "@repo/web-ui/components/icons/add-to
 import { ChevronRight } from "@repo/web-ui/components/icons/chevron-right";
 import { Minus } from "@repo/web-ui/components/icons/minus";
 import { Plus } from "@repo/web-ui/components/icons/plus";
-import { Badge } from "@repo/web-ui/components/ui/badge";
 import { Button } from "@repo/web-ui/components/ui/button";
 import {
   Collapsible,
@@ -387,21 +386,28 @@ const LocationStocks = ({
   const firstLocation = checkAvailabilityQuery.data.availableLocations[0];
   const otherLocations =
     checkAvailabilityQuery.data.availableLocations.slice(1);
+  const isBackordered = checkAvailabilityQuery.data.status === "notInStock";
 
   return (
     <Collapsible className="flex flex-col gap-1">
       <div className="flex flex-row items-center justify-between gap-2 rounded-lg bg-wurth-gray-50 p-2">
-        <div className="flex flex-row items-center gap-2">
-          <Badge
-            variant="success-alt"
-            className="rounded px-2 py-1 text-sm font-semibold leading-4 shadow-none"
+        <div className="flex shrink-0 flex-row items-center gap-2">
+          <div
+            className={cn(
+              "rounded px-4 py-2 text-sm font-semibold leading-4 md:px-2 md:py-1",
+              isBackordered
+                ? "bg-yellow-50 text-yellow-700"
+                : "bg-green-50 text-green-700",
+            )}
           >
-            In Stock
-          </Badge>
-
-          <div className="text-sm font-medium text-wurth-gray-800">
-            {firstLocation?.amount} in stock at {firstLocation?.name}
+            {isBackordered ? "Backordered" : "In Stock"}
           </div>
+
+          {!isBackordered && (
+            <div className="text-sm font-medium text-wurth-gray-800">
+              {firstLocation?.amount} in stock at {firstLocation?.name}
+            </div>
+          )}
         </div>
 
         <CollapsibleTrigger
