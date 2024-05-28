cb75d7b5d4e9601022b0737a4ac1515721a19357
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-05-28 11:14:46 +0530

    WXM-407: Update shared availablity check

--- apps/lac-web/app/cart/_cart-list/use-check-availability-mutation.hook.ts
+++ apps/lac-web/app/cart/_cart-list/use-check-availability-mutation.hook.ts
@@ -1,78 +1,11 @@
-import { api } from "@/_lib/api";
-import type { ShippingMethod } from "@/_lib/types";
+import { checkAvailability } from "@/_lib/apis/shared";
+import type { AvailabilityParameters, Token } from "@/_lib/types";
 import { useMutation } from "@tanstack/react-query";
 
-type CheckAvailability = {
-  productid: number;
-  status: string;
-  options: {
-    backOrder: boolean;
-    plants: {
-      index: number;
-      isSameDayAvail: boolean;
-      plant: string;
-      quantity?: number;
-      backOrderQuantity?: number;
-      backOrderDate?: string;
-      shippingMethods: ShippingMethod[];
-    }[];
-    type: string;
-    hash: string;
-  }[];
-  willcallanywhere: {
-    hash: string;
-    status: string;
-    willCallBackOrder: string; // Back order date
-    willCallPlant: string;
-    willCallQuantity: number;
-    backOrder?: boolean;
-    backOrderDate_1?: string;
-    backOrderQuantity_1?: number;
-    index?: number;
-    plant_1?: string;
-    quantity_1?: number;
-    shippingMethods_1?: string[];
-    type?: string;
-  };
-  xplant: string;
-  available_locations: {
-    location: string;
-    name: string;
-    amount: number;
-  }[];
-};
-
-type AvailabilityParams = {
-  productId: number;
-  qty: number;
-  plant?: string;
-};
-
-const useCheckAvailabilityMutation = (token: string) => {
+const useCheckAvailabilityMutation = (token: Token) => {
   return useMutation({
-    mutationFn: async ({ productId, qty, plant }: AvailabilityParams) => {
-      const response = await api
-        .post("rest/availability-check", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          json: {
-            productid: productId,
-            qty,
-            plant,
-          },
-          cache: "no-store",
-        })
-        .json<CheckAvailability>();
-
-      return {
-        productId: response.productid,
-        status: response.status,
-        options: response.options,
-        willCallAnywhere: response.willcallanywhere,
-        xplant: response.xplant,
-        availableLocations: response.available_locations,
-      };
+    mutationFn: async ({ productId, qty, plant }: AvailabilityParameters) => {
+      return await checkAvailability(token, { productId, qty, plant });
     },
   });
 };
