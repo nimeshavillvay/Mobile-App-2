3acaddbdc586b0f671835887be0d55592d070056
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-05-31 20:27:15 +0530

    WXM-595: Update font size

--- apps/lac-web/app/cart/cart-heading.tsx
+++ apps/lac-web/app/cart/cart-heading.tsx
@@ -10,7 +10,7 @@ const CartHeading = ({ token }: CartHeadingProps) => {
   const { data } = useSuspenseCart(token);
 
   return (
-    <h1 className="container mb-6 mt-4 text-2xl font-light tracking-[-0.144px] text-wurth-gray-800 md:mb-7 md:mt-6 md:text-[40px] md:leading-[3.5rem] md:tracking-[-0.576px]">
+    <h1 className="container mb-6 mt-4 text-2xl font-light tracking-[-0.144px] text-wurth-gray-800 md:mb-7 md:mt-6 md:text-[2.5rem] md:leading-[3rem] md:tracking-[-0.576px]">
       <span className="font-medium">Cart</span> ({data.cartItems.length}{" "}
       {data.totalQuantity === 1 ? "item" : "items"})
     </h1>
