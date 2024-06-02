8aea817b5dde69f1b26707f82268c85a2adbc195
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2024-06-02 12:07:25 +0530

    WXM-536: Cart item price edit for osr

--- apps/lac-web/app/cart/_cart-list/cart-item-price.tsx
+++ apps/lac-web/app/cart/_cart-list/cart-item-price.tsx
@@ -1,11 +1,12 @@
 import useSuspensePriceCheck from "@/_hooks/product/use-suspense-price-check.hook";
 import useSuspenseCheckLogin from "@/_hooks/user/use-suspense-check-login.hook";
-import { Token } from "@/_lib/types";
+import type { Token } from "@/_lib/types";
 import { cn, formatNumberToPrice } from "@/_lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@repo/web-ui/components/ui/input";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { ViewportTypes } from "./types";
+import type { ViewportTypes } from "./types";
 
 type CartItemPriceProps = {
   readonly quantity: number;
@@ -15,7 +16,12 @@ type CartItemPriceProps = {
 };
 
 const cartItemPriceSchema = z.object({
-  quantity: z.number(),
+  price: z
+    .union([
+      z.string().transform((x) => x.replace(/[^0-9.-]+/g, "")),
+      z.number(),
+    ])
+    .pipe(z.coerce.number().min(0.0001).max(999999999)),
 });
 
 const CartItemPrice = ({
@@ -32,6 +38,15 @@ const CartItemPrice = ({
   const priceData = priceCheckQuery.data.productPrices[0];
   const loginCheckData = loginCheckResponse.data;
 
+  const { register, formState } = useForm<z.infer<typeof cartItemPriceSchema>>({
+    resolver: zodResolver(cartItemPriceSchema),
+    values: {
+      price: priceData?.price ?? 0,
+    },
+  });
+
+  console.log(formState.errors);
+
   if (
     loginCheckData.status_code === "OK" &&
     "sales_rep_id" in loginCheckData &&
@@ -45,8 +60,12 @@ const CartItemPrice = ({
         )}
       >
         <Input
-          className="h-fit w-24 rounded-none rounded-l border-r-0 px-2.5 py-1 text-right text-base focus:border-none md:w-20"
+          className={cn(
+            "h-fit w-24 rounded-none rounded-l border-r-0 px-2.5 py-1 text-right text-base md:w-20",
+            formState.errors.price ? "border-red-700" : "border-green-700",
+          )}
           type="number"
+          {...register("price")}
         />
         <span className="rounded-r border border-l-0 p-1 pr-1.5 lowercase text-wurth-gray-400">
           /{priceData?.priceUnit}
