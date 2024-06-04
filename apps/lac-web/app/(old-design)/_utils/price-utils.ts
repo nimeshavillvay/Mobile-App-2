f1f80f2a7a840123128627e7afd85045d35f81a6
Shyamali <shyamali@villvay.com>
2024-06-05 00:47:45 +0530

    WXM-657: re-correcting price logic in my purchase items

--- apps/lac-web/app/(old-design)/_utils/price-utils.ts
+++ apps/lac-web/app/(old-design)/_utils/price-utils.ts
@@ -1,12 +1,5 @@
-export const calculatePriceDetails = (price: number, salePrice: number) => {
-  const displayPrice = salePrice > 0 && price > salePrice ? salePrice : price;
-  const originalPrice = salePrice > 0 && price > salePrice ? price : 0;
-  const savingAmount =
-    salePrice > 0 && price > salePrice ? price - salePrice : 0;
-  const discount =
-    salePrice > 0 && price > salePrice
-      ? (((price - salePrice) / price) * 100).toFixed(0.5)
-      : 0;
-
-  return { displayPrice, originalPrice, savingAmount, discount };
+export const calculatePriceDetails = (price: number, listPrice: number) => {
+  const displayPrice = price > listPrice ? listPrice : price;
+  const discount = Math.round(((listPrice - price) / listPrice) * 100);
+  return { displayPrice, discount };
 };
--- apps/lac-web/app/(old-design)/myaccount/purchaseditems/_item-prices/unit-price-row.tsx
+++ apps/lac-web/app/(old-design)/myaccount/purchaseditems/_item-prices/unit-price-row.tsx
@@ -3,24 +3,21 @@ import { calculatePriceDetails } from "@/old/_utils/price-utils";
 
 type UnitPriceRowProps = {
   readonly price: number;
-  readonly salePrice: number;
+  readonly listPrice: number;
   readonly uom: string;
 };
 
 // TODO: Can be reusable in other components
-const UnitPriceRow = ({ price, salePrice, uom }: UnitPriceRowProps) => {
-  const { displayPrice, originalPrice, savingAmount, discount } =
-    calculatePriceDetails(price, salePrice);
+const UnitPriceRow = ({ price, listPrice, uom }: UnitPriceRowProps) => {
+  const { displayPrice, discount } = calculatePriceDetails(price, listPrice);
 
   return (
     <div className="flex flex-row justify-end gap-1 px-4">
       <div className="font-bold">${formatNumberToPrice(displayPrice)}</div>
       <div>/ {uom ?? ""}</div>
-      {savingAmount > 0 && (
+      {discount > 0 && (
         <>
-          <div className="line-through">
-            ${formatNumberToPrice(originalPrice)}
-          </div>
+          <div className="line-through">${formatNumberToPrice(listPrice)}</div>
           <div className="rounded-md bg-brand-success/10 px-1.5 font-bold text-brand-success">
             {discount}% off
           </div>
