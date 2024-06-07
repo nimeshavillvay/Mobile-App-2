fa97a16ca31652f7f5a177295de0bd7e40ef4c8a
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-06-07 23:23:00 +0530

    WXM-734: Added check for missing payment option

--- apps/lac-web/app/checkout/_billing-and-payment-info/order-confirm-dialog.tsx
+++ apps/lac-web/app/checkout/_billing-and-payment-info/order-confirm-dialog.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSuspenseCart from "@/_hooks/cart/use-suspense-cart.hook";
+import type { PaymentMethod } from "@/_lib/types";
 import { Button } from "@repo/web-ui/components/ui/button";
 import {
   Dialog,
@@ -11,18 +12,50 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@repo/web-ui/components/ui/dialog";
+import { useToast } from "@repo/web-ui/components/ui/toast";
 import { useState } from "react";
+import { getPaymentId } from "./helpers";
 import useCheckoutMutation from "./use-checkout-mutation.hook";
+import useSuspenseCreditCards from "./use-suspense-credit-cards.hook";
 
-const OrderConfirmDialog = ({ token }: { readonly token: string }) => {
+type OrderConfirmDialog = {
+  readonly token: string;
+  readonly otherPaymentMethods: PaymentMethod[];
+};
+
+const OrderConfirmDialog = ({
+  token,
+  otherPaymentMethods,
+}: OrderConfirmDialog) => {
+  const { toast } = useToast();
   const [open, setOpen] = useState(false);
   const [buttonDisable, setButtonDisable] = useState(false);
+
+  const cartQuery = useSuspenseCart(token);
+  const creditCartsQuery = useSuspenseCreditCards(token);
+
+  const selectedPaymentId = getPaymentId(
+    {
+      paymentToken: cartQuery.data.configuration.paymentToken,
+      paymentMethod: cartQuery.data.configuration.paymentMethod,
+    },
+    { creditCards: creditCartsQuery.data, paymentMethods: otherPaymentMethods },
+  );
+
   const checkoutMutation = useCheckoutMutation();
-  const cartMutation = useSuspenseCart(token);
 
   const clickConfirmOrder = () => {
-    checkoutMutation.mutate();
-    setButtonDisable(true);
+    // Check if a valid payment method is selected
+    if (selectedPaymentId) {
+      checkoutMutation.mutate();
+      setButtonDisable(true);
+    } else {
+      setOpen(false);
+      toast({
+        variant: "destructive",
+        title: "Please select a payment method",
+      });
+    }
   };
 
   return (
@@ -32,7 +65,7 @@ const OrderConfirmDialog = ({ token }: { readonly token: string }) => {
           variant="secondary"
           size="lg"
           className="h-fit rounded-lg px-20 py-4 text-lg font-normal shadow-md md:max-w-60 md:self-end"
-          disabled={cartMutation.data.allRegionalExluded}
+          disabled={cartQuery.data.allRegionalExluded}
         >
           Place your Order
         </Button>
