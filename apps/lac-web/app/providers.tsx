b9d3636a1a989283fa06e4776d2a4241a5357974
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-06-25 14:14:02 +0530

    WXM-847: Redirect to sign in page when trying to mutate without a session token

--- apps/lac-web/app/providers.tsx
+++ apps/lac-web/app/providers.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { revalidateSiteLayout } from "@/_actions/revalidate";
+import { SESSION_TOKEN_COOKIE } from "@/_lib/constants";
 import { useToast } from "@repo/web-ui/components/ui/toast";
 import { TooltipProvider } from "@repo/web-ui/components/ui/tooltip";
 import {
@@ -9,8 +11,8 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-import { useRouter } from "next/navigation";
-import { useState, type ReactNode } from "react";
+import Cookies from "js-cookie";
+import { useRef, useState, type ReactNode } from "react";
 import { CookiesProvider } from "react-cookie";
 import { Provider as WrapBalancer } from "react-wrap-balancer";
 
@@ -19,8 +21,26 @@ type ProvidersProps = {
 };
 
 const Providers = ({ children }: ProvidersProps) => {
-  const router = useRouter();
+  // This ref is used to prevent multiple session expire toasts from showing up
+  // and multiple redirects from happening
+  const isHandlingSessionExpireRef = useRef(false);
   const { toast } = useToast();
+
+  const handleNoSession = async () => {
+    if (!isHandlingSessionExpireRef.current) {
+      isHandlingSessionExpireRef.current = true;
+
+      toast({
+        title: "Session expired",
+        description: "Please sign in again",
+        variant: "destructive",
+      });
+
+      await revalidateSiteLayout("/sign-in");
+
+      isHandlingSessionExpireRef.current = false;
+    }
+  };
 
   const [queryClient] = useState(
     () =>
@@ -33,17 +53,20 @@ const Providers = ({ children }: ProvidersProps) => {
           },
         },
         mutationCache: new MutationCache({
+          onSettled: () => {
+            const sessionCookie = Cookies.get(SESSION_TOKEN_COOKIE);
+
+            // If there is no session token, there's no point in the mutation as the
+            // data won't be saved on the server
+            if (!sessionCookie) {
+              handleNoSession();
+            }
+          },
           onError: (error) => {
             // Redirect the user to the sign in page if they try to do a mutation
             // after the token has expired
             if (error?.response?.status === 403) {
-              toast({
-                title: "Session expired",
-                description: "Please sign in again",
-                variant: "destructive",
-              });
-
-              router.replace("/sign-in");
+              handleNoSession();
             }
           },
         }),
