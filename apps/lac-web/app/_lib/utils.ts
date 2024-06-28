f4695235d11d63366a723b26640aeb078a66e5b4
Shyamali <shyamali@villvay.com>
2024-06-28 18:25:55 +0530

    WXM-979: change shipping address msg

--- apps/lac-web/app/_lib/utils.ts
+++ apps/lac-web/app/_lib/utils.ts
@@ -22,7 +22,8 @@ type ErrorResponse = {
 export const isErrorResponse = (error: unknown): error is ErrorResponse => {
   if (
     typeof error === "object" &&
-    typeof (error as ErrorResponse)?.status_code === "string" &&
+    (typeof (error as ErrorResponse)?.status_code === "string" ||
+      (error as ErrorResponse)?.status_code === undefined) &&
     typeof (error as ErrorResponse)?.message === "string"
   ) {
     return true;
