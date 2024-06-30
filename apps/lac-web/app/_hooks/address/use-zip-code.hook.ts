a389ff4ea0619c8d2f2fe1ee2a491d60e110a155
Arshad <arshad@Admins-MacBook-Pro.local>
2024-06-30 18:19:29 +0530

    imported types as types

--- apps/lac-web/app/_hooks/address/use-zip-code.hook.ts
+++ apps/lac-web/app/_hooks/address/use-zip-code.hook.ts
@@ -1,4 +1,5 @@
-import { ChangeEvent, useState } from "react";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
 
 export const useZipCodeFormatter = () => {
   const [zipCode, setZipCode] = useState<string | undefined>("");
