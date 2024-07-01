23347df97598256734d1b4980025716a4d3f3609
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-07-01 11:26:26 +0530

    WXM-960: Increase gap between attributes

--- apps/lac-web/app/(old-design)/myaccount/purchaseditems/_item-attributes/item-attributes.tsx
+++ apps/lac-web/app/(old-design)/myaccount/purchaseditems/_item-attributes/item-attributes.tsx
@@ -11,32 +11,36 @@ const ItemAttributes = ({ productId }: ItemAttributesProps) => {
   const attributes = itemAttributesQuery.data?.[0] ?? null;
 
   return (
-    <div className="flex flex-row gap-2 py-2 text-sm text-brand-gray-500">
+    <div className="flex flex-row gap-6 py-2 text-sm text-brand-gray-500">
       {attributes ? (
         <>
-          <div className="flex-1 flex-col">
-            <Attribute label="Brand" value={attributes.brand} />
-            <Attribute
-              label="Box Quantity"
-              value={attributes.boxQuantity.toString()}
-            />
-            <Attribute
-              label="Manufacturer&rsquo;s Part"
-              value={attributes.mfrPartNo}
-            />
-            <Attribute label="Weight" value={attributes.weight.toString()} />
-          </div>
+          <table className="h-fit flex-1">
+            <tbody>
+              <Attribute label="Brand" value={attributes.brand} />
+              <Attribute
+                label="Box Quantity"
+                value={attributes.boxQuantity.toString()}
+              />
+              <Attribute
+                label="Manufacturer&rsquo;s Part"
+                value={attributes.mfrPartNo}
+              />
+              <Attribute label="Weight" value={attributes.weight.toString()} />
+            </tbody>
+          </table>
 
-          <div className="flex-1 flex-col">
-            {attributes.attributes?.length > 0 &&
-              attributes.attributes.map((attribute) => (
-                <Attribute
-                  key={attribute.attribute_name}
-                  label={attribute.attribute_name}
-                  value={attribute.attribute_value ?? ""}
-                />
-              ))}
-          </div>
+          <table className="flex-1">
+            <tbody>
+              {attributes.attributes?.length > 0 &&
+                attributes.attributes.map((attribute) => (
+                  <Attribute
+                    key={attribute.attribute_name}
+                    label={attribute.attribute_name}
+                    value={attribute.attribute_value ?? ""}
+                  />
+                ))}
+            </tbody>
+          </table>
         </>
       ) : (
         itemAttributesQuery.isLoading && <Skeleton className="h-32 w-full" />
@@ -59,9 +63,9 @@ const Attribute = ({
   }
 
   return (
-    <div className="flex flex-row">
-      <div className="flex-1 font-bold">{label}:</div>
-      <div className="flex-1" dangerouslySetInnerHTML={{ __html: value }} />
-    </div>
+    <tr>
+      <td className="font-bold">{label}:</td>
+      <td className="pl-4" dangerouslySetInnerHTML={{ __html: value }} />
+    </tr>
   );
 };
