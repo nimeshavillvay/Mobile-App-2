2ea60c1a089049e4157f4b6b196733caecb0b2e9
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-07-08 18:15:49 +0530

    More changes to testing config

--- apps/lac-mobile/jest.config.ts
+++ apps/lac-mobile/jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   transformIgnorePatterns: [
     "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))",
   ],
-  setupFilesAfterEnv: ["./jest-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
 };
 
 export default config;
