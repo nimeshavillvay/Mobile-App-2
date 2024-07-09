2fe3e11a43824a89054ee79da840e3819b4c4368
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-07-09 10:20:46 +0530

    Added tests to WWE web

---
+++ apps/wwe-web/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/app/$1",
+    "^~/(.*)$": "<rootDir>/../../packages/web-ui/src/$1",
+  },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(customJestConfig);
