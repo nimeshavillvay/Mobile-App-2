d8c1c9d4f94aa1f05dcf6c55a137eb82ce52f98f
Frank Alwis <frank.alwis@villvay.com>
2024-07-10 15:58:27 +0530

    pr review changes

--- apps/lac-web/app/cart/_cart-list/cart-item-not-available-banner.tsx
+++ apps/lac-web/app/cart/_cart-list/cart-item-not-available-banner.tsx
@@ -14,19 +14,24 @@ const NotAvailableInfoBanner = ({
   readonly willCallType: string;
   readonly willCallTransferPlant?: string;
 }) => {
-  return willCallType && willCallType === MAIN_OPTIONS.WILL_CALL_TRANSFER ? (
-    <div className="flex flex-col items-center gap-1 rounded-xl bg-red-800/10 px-4 py-2 text-sm">
-      <div className="text-red-800">
-        This item is not available for transfer from{" "}
-        <PlantName plants={plants} plantCode={willCallPlant} /> to{" "}
-        <PlantName plants={plants} plantCode={willCallTransferPlant ?? "N/A"} />
+  if (willCallType === MAIN_OPTIONS.WILL_CALL_TRANSFER) {
+    return (
+      <div className="flex flex-col items-center gap-1 rounded-xl bg-red-800/10 px-4 py-2 text-sm">
+        <div className="text-red-800">
+          This item is not available for transfer from{" "}
+          <PlantName plants={plants} plantCode={willCallPlant} /> to{" "}
+          <PlantName
+            plants={plants}
+            plantCode={willCallTransferPlant ?? "N/A"}
+          />
+        </div>
+        <div className="text-xs text-wurth-gray-500">
+          To proceed, please select a valid shipping option.
+        </div>
       </div>
-      <div className="text-xs text-wurth-gray-500">
-        To proceed, please select a valid shipping option.
-      </div>
-    </div>
-  ) : (
-    willCallType && willCallType === MAIN_OPTIONS.WILL_CALL && (
+    );
+  } else if (willCallType === MAIN_OPTIONS.WILL_CALL) {
+    return (
       <div className="flex flex-col items-center gap-1 rounded-xl bg-red-800/10 px-4 py-2 text-sm">
         <div className="text-red-800">
           This item is not available for pickup at{" "}
@@ -36,8 +41,10 @@ const NotAvailableInfoBanner = ({
           To proceed, please select a valid shipping option.
         </div>
       </div>
-    )
-  );
+    );
+  }
+
+  return null;
 };
 
 export default NotAvailableInfoBanner;
