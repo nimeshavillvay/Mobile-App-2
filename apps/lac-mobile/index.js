9dd28e71e880b32ebb2647b413f346a7c3e72a02
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-07-16 09:48:25 +0530

    Workaround for the default Expo Router entrypoint not working on Android

---
+++ apps/lac-mobile/index.js
@@ -0,0 +1,3 @@
+// Workaround for the default Expo Router entrypoint not working on Android
+// https://github.com/t3-oss/create-t3-turbo/issues/741#issuecomment-1812454342
+import "expo-router/entry";
