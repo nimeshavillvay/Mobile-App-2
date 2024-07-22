7ea032df0c9482595d59d26e0a3f23fee22a132b
Shyamali <shyamali@villvay.com>
2024-07-22 09:49:44 +0530

    WNXM-1031: CR1 and CR2

--- apps/lac-web/app/search/page.tsx
+++ apps/lac-web/app/search/page.tsx
@@ -118,7 +118,7 @@ const SearchPage = async ({
             <h1 className="container line-clamp-3 text-balance font-title text-4xl font-medium tracking-tight text-wurth-gray-800 md:text-5xl md:leading-[3.5rem] md:tracking-[-0.036rem]">
               Search Results for &quot;{query}&quot;
             </h1>
-            <div className="mx-auto max-w-[1360px]">
+            <div className="mx-auto">
               <Suspense
                 fallback={
                   <ProductsGrid>
