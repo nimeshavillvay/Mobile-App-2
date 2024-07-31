97878017862057c8e35ebfdc4e017ef9f550f243
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-07-31 13:46:52 +0530

    Revert "Temp"
    This reverts commit 2c77a5ce2cf779a94dc39e29bb8a43a03695676f.

--- apps/lac-mobile/app/(tabs)/index.tsx
+++ apps/lac-mobile/app/(tabs)/index.tsx
@@ -7,18 +7,6 @@ const HomePage = () => {
     <ScreenLayout edges={["top", "right", "left"]}>
       <ScreenHeader title="Home" />
       <Text>Home Page</Text>
-
-      <Text>
-        EXPO_PUBLIC_WURTH_LAC_API: {process.env.EXPO_PUBLIC_WURTH_LAC_API}
-      </Text>
-      <Text>
-        EXPO_PUBLIC_WURTH_LAC_API_KEY:{" "}
-        {process.env.EXPO_PUBLIC_WURTH_LAC_API_KEY}
-      </Text>
-      <Text>
-        EXPO_PUBLIC_WURTH_LAC_SEARCH_API:{" "}
-        {process.env.EXPO_PUBLIC_WURTH_LAC_SEARCH_API}
-      </Text>
     </ScreenLayout>
   );
 };
