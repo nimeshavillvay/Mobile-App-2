641a8795e86d879d9ea95dbf195442708004988e
arshaqvill <124868771+arshaqvill@users.noreply.github.com>
2024-07-31 12:53:18 -0600

    feat: apply wlac theming for wbs (#519)
    Co-authored-by: arshaqsoul <arshaqsoul@gmail.com>

--- apps/wbs-web/tailwind.config.ts
+++ apps/wbs-web/tailwind.config.ts
@@ -7,7 +7,26 @@ const config: Config = {
     "!../../packages/web-ui/src/**/*.stories.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#cc0000",
+          secondary: "#00adef",
+          tertiary: "#c3cf23",
+          success: "#55a213",
+          gray: {
+            100: "#f7f7f7",
+            200: "#dedede",
+            300: "#bdbdbd",
+            400: "#959595",
+            500: "#605d5c",
+          },
+        },
+      },
+      maxWidth: {
+        desktop: "var(--desktop-width)",
+      },
+    },
   },
   presets: [require("@repo/tailwindcss-config")],
   plugins: [],
