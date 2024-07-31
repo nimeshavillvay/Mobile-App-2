b95ed22577c9e52073d9eda04cd4f255d01a5f80
Frank Alwis <97273148+FrankAlwis24@users.noreply.github.com>
2024-08-01 00:23:57 +0530

    basic atoms skeleton calender and textarea (#518)

---
+++ packages/web-ui/src/components/base/atoms/calendar/calendar.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+import { Calendar } from "./calendar";
+
+const meta: Meta<typeof Calendar> = {
+  title: "Base/Atom/Calendar",
+  component: Calendar,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Calendar>;
+
+export const Showcase: Story = {
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [date, setDate] = useState<Date | undefined>(new Date());
+
+    return (
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow"
+      />
+    );
+  },
+};
---
+++ packages/web-ui/src/components/base/atoms/skeleton/skeleton.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Skeleton } from "./skeleton";
+
+const meta: Meta<typeof Skeleton> = {
+  title: "Base/Atom/Skeleton",
+  component: Skeleton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Skeleton>;
+
+export const Showcase: Story = {
+  render: () => {
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    );
+  },
+};
+
+export const SkeletonCard: Story = {
+  render: () => (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  ),
+};
