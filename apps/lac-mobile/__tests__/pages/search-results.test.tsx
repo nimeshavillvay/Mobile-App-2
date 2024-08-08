d8456f1712c6c11c651daff721d1e2e1a7191ded
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-08-08 10:45:08 +0530

    WGM-65: Hide errors for Expo image in search results page tests

--- apps/lac-mobile/__tests__/pages/search-results.test.tsx
+++ apps/lac-mobile/__tests__/pages/search-results.test.tsx
@@ -19,6 +19,23 @@ describe("Search Results", () => {
     server.close();
   });
 
+  // TODO Remove workaround after upgrading Expo and jest-expo
+  // Suppress console.error because of this error "Warning: Unexpected ref object provided for ExpoImage. Use either a ref-setter function or React.createRef()."
+  // https://github.com/expo/expo/issues/28831
+  beforeEach(() => {
+    jest.spyOn(console, "error");
+    // @ts-expect-error jest.spyOn adds this functionality
+    console.error.mockImplementation(() => null);
+  });
+
+  // TODO Remove workaround after upgrading Expo and jest-expo
+  // Suppress console.error because of this error "Warning: Unexpected ref object provided for ExpoImage. Use either a ref-setter function or React.createRef()."
+  // https://github.com/expo/expo/issues/28831
+  afterEach(() => {
+    // @ts-expect-error jest.spyOn adds this functionality
+    console.error.mockRestore();
+  });
+
   it("shows the search results when given the search parameter", async () => {
     renderRouter(
       { "search-results": SearchResultsPage },
