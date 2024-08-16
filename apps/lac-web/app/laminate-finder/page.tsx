7c95a4a6cdc6724c65c308ecf8ed0f981880a975
Chathura Kudahetti <chathura@villvay.com>
2024-08-16 16:51:54 +0530

    fix breadcrumb top margin

--- apps/lac-web/app/laminate-finder/page.tsx
+++ apps/lac-web/app/laminate-finder/page.tsx
@@ -24,7 +24,7 @@ const Page = () => {
   return (
     <>
       {/* container my-14 space-y-3 md:my-20 md:space-y-6 */}
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-3">
         <div className="mb-6">
           <Breadcrumb>
             <BreadcrumbList>
