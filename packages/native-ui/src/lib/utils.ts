bd8018e04c42e3ee4d4e3ec1d052f30031416f35
Arshad <arshad@192.168.1.18>
2024-08-22 18:55:48 +0530

    added default value for alpha in rgba conversion

--- packages/native-ui/src/lib/utils.ts
+++ packages/native-ui/src/lib/utils.ts
@@ -1,4 +1,4 @@
-export const convertToRGBA = (color: string, alpha: number): string => {
+export const convertToRGBA = (color: string, alpha: number = 1): string => {
   const hexToRGB = (hex: string): number[] => {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
