7dd70330f72954d28b2c8a8032d94cca254d5054
Shyamali <shyamali@villvay.com>
2024-09-12 15:45:43 +0530

    WNXM-1230: fix build issue

--- apps/lac-web/app/(old-design)/myaccount/shopping-lists/use-remove-shopping-list-item-mutation.hook.ts
+++ apps/lac-web/app/(old-design)/myaccount/shopping-lists/use-remove-shopping-list-item-mutation.hook.ts
@@ -1,4 +1,4 @@
-import { getGtmProducts } from "@/_hooks/gtm/use-gtm-item-info.hook";
+import { getGtmProducts } from "@/_hooks/gtm/use-gtm-item-info";
 import { getGtmUser } from "@/_hooks/gtm/use-gtm-user.hook";
 import useCookies from "@/_hooks/storage/use-cookies.hook";
 import { api } from "@/_lib/api";
