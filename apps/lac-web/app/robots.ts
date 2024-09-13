327ce3a659a17d8a705b3a894d2417ce6d4a6ccc
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-09-13 15:27:41 +0530

    Added more rules for bots

--- apps/lac-web/app/robots.ts
+++ apps/lac-web/app/robots.ts
@@ -3,21 +3,27 @@ import type { MetadataRoute } from "next";
 
 const robots = (): MetadataRoute.Robots => {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/api/",
-        "/myaccount/",
-        "/cart/",
-        "/checkout/",
-        "/confirmation/",
-        "/osr/",
-        "/email/subscription-confirmation",
-        "/search/",
-        "/laminate-finder/",
-      ],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/myaccount/",
+          "/cart/",
+          "/checkout/",
+          "/confirmation/",
+          "/osr/",
+          "/email/subscription-confirmation/",
+          "/search/",
+          "/laminate-finder/",
+        ],
+      },
+      {
+        userAgent: "MJ12bot",
+        disallow: "/",
+      },
+    ],
     host: PRODUCTION_URL,
     sitemap: `${PRODUCTION_URL}/sitemap.xml`,
   };
--- apps/lac-web/middleware.ts
+++ apps/lac-web/middleware.ts
@@ -133,8 +133,10 @@ export const config = {
      * and those containing these in the pathname:
      * - sitemap (sitemap files)
      * - opengraph-image (Open Graph images)
+     * - .html (HTML files)
+     * - .php (PHP files)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|.*sitemap|.*opengraph-image|monitoring|autodiscover/autodiscover.xml).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|.*sitemap|.*opengraph-image|monitoring|autodiscover/autodiscover.xml|.*html|.*php).*)",
   ],
 };
 
