2ff02c948b1766e40e13806d1a446e7132c9d5b6
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-09-19 14:21:52 +0530

    Send token for price check

--- apps/lac-web/app/(site)/product/[id]/[slug]/_related-products-list/products-list.tsx
+++ apps/lac-web/app/(site)/product/[id]/[slug]/_related-products-list/products-list.tsx
@@ -13,7 +13,7 @@ type ProductsListProps = {
 
 const ProductsList = ({ token, products }: ProductsListProps) => {
   const priceCheckQuery = useSuspensePriceCheck(
-    undefined,
+    token,
     products.map((product) => ({
       productId: Number(product.productid),
       qty: 1,
