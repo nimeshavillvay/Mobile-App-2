1ccbb4507284e6fa540f23d559f2e19db6ea419f
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2024-09-24 22:09:37 +0530

    Fixed issues with sitemap

--- apps/lac-web/app/assets/sitemap.xml/route.ts
+++ apps/lac-web/app/assets/sitemap.xml/route.ts
@@ -1,5 +1,5 @@
 import {
-  getFullUrl,
+  SitemapIndex,
   getNumberOfPages,
   getSitemapAssets,
 } from "@/_lib/sitemap-helpers";
@@ -8,25 +8,14 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   const sitemapAssets = await getSitemapAssets();
 
-  const urls: string[] = [];
+  const sitemapIndex = new SitemapIndex();
 
   const numberOfAssetPages = getNumberOfPages(sitemapAssets);
   Array.from({ length: numberOfAssetPages }).forEach((_, index) => {
-    urls.push(getFullUrl(`/assets/sitemaps/sitemap/${index}.xml`));
+    sitemapIndex.addUrl(`/assets/sitemaps/sitemap/${index}.xml`);
   });
 
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
-  urls.forEach((url) => {
-    xml += "<sitemap>";
-    xml += `<loc>${url}</loc>`;
-    xml += "</sitemap>";
-  });
-
-  xml += "</urlset>";
-
-  return new NextResponse(xml, {
+  return new NextResponse(sitemapIndex.toString(), {
     status: 200,
     headers: {
       "Content-Type": "application/xml",
